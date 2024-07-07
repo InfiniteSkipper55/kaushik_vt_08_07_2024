d9616cc31a202ee311f7d5f8594957cc8b42a74a
InfiniteSkipper55 <kaushik.rao09@gmail.com>
2024-07-07 20:31:23 +0530

    removed unnecessary annotations

--- src/main/java/com/skipper/UrlShortenerApplication.java
+++ src/main/java/com/skipper/UrlShortenerApplication.java
@@ -2,12 +2,8 @@ package com.skipper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.skipper.repository")
-@EntityScan(basePackages = "com.skipper.model")
 public class UrlShortenerApplication {
 
 	public static void main(String[] args) {
